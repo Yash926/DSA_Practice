8d8dc641f5e7bbf42898b6c71cfb23517fd885cf
Yash Tripathi <114800027+Yash926@users.noreply.github.com>
2024-01-12 22:53:23 +0530

    Basic Array

---
+++ array_1.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int x[5];
+//	x[2]=7;
+//	x[10]=9;
+//	cout<<x[10]<<endl;
+//	cout<<x[2]<<endl;
+//}
+
+
+// array traversing
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x[5]={2,4,7,8,9};
+	for(int i=0;i<5;i++)// forward traversing
+	cout<<x[i]<<endl;
+}
